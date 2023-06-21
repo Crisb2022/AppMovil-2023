033ada52de3212efdc28e427c7260d30754081fa
Crisb2022 <122027767+Crisb2022@users.noreply.github.com>
2023-06-21 12:51:36 +0000

    Clase

--- frontend/app-ejemplo/App.js
+++ frontend/app-ejemplo/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from "./src/Navigation.js";
 import { NavigationContainer} from '@react-navigation/native';
+import ProfileCard from './src/components/home/ProfileCard.js';
 
 export default function App() {
 
