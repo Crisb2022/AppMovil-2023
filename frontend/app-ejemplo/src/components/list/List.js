23ed7d0237d95d69c30d2f12bd2ed61d04a239a2
Crisb2022 <122027767+Crisb2022@users.noreply.github.com>
2023-06-23 12:32:22 +0000

    Commit Perfecto

--- frontend/app-ejemplo/src/components/list/List.js
+++ frontend/app-ejemplo/src/components/list/List.js
@@ -1,45 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { Touchable, TouchableOpacity } from "react-native";
-import { FlatList, View, StyleSheet, Text } from "react-native";
+import Task from "./Task";
+import { FlatList, View, StyleSheet, Text, Item } from "react-native";
 
 
 
-const Listcomponent= () =>{
+const Listcomponent = (props) => {
 
     const [taskItems, setTaskItems] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchData();
     }, [])
 
-    const fetchData = async () =>{
+    const fetchData = async () => {
         try {
             const response = await fetch('https://api.unsplash.com/photos/?client_id=ZXjOAAdwefwfYGtyhjJmAerkWnGDxNNnEwTlnHkSqk4');
             const jsonData = await response.json();
-            console.log(jsonData)
-            setTaskItems(jsonData)   
+            console.log("------------------------------- ", jsonData)
+            setTaskItems(jsonData)
         } catch (error) {
             console.log("error: ", error)
         }
     }
 
-    const item = ({task, i}) =>{
+    const Item = ({ task, i }) => {
         return (
-            <TouchableOpacity style={styles.peritem} key={i} onPress={() =>{getProfile(task)}}>
-                <Task task={task}/>
+            <TouchableOpacity style={styles.peritem} key={i} onPress={() => { getProfile(task) }}>
+                <Task task={task} />
             </TouchableOpacity>
         )
     }
 
-    return( taskItems &&
+    return (taskItems &&
         <View style={styles.container}>
-            <View style={styles.taskWrapper}> 
+            <View style={styles.taskWrapper}>
                 <Text style={styles.sectiontitle}>Se listan los perfiles</Text>
                 <View style={styles.items}>
-                    <FlatList>
-                        data = {taskItems}
-                        renderitem={({item,i}) => <Item task={item} i={i}/>}
-                    </FlatList>
+                    <FlatList data={taskItems}
+                        renderItem={({ item, i }) =>
+                            <Item task={item} i={i} />
+                        }>
+
+                        </FlatList>
                 </View>
             </View>
         </View>
